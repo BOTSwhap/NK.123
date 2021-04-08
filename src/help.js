2c1eae2148eb3bf64d7504f1ac048c67707e988b
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-04-07 20:46:05 -0500

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,9 @@
+const help = (prefix) => {
+	return `Por favor use el comando:
+[.ayuda] 
+para mostrar los comandos con con ejm completo, o use el comando
+[.comandos] 
+para mostrar solo comandos UwU`
+}
+
+exports.help = help
