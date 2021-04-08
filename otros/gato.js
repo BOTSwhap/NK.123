c664f3fffd863dbea90cf808751443bb43e5bf90
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-04-07 20:51:01 -0500

    Update gato.js

--- otros/gato.js
+++ otros/gato.js
@@ -1,6 +1,3 @@
 [
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRJtqk_wKfcyv2WCKyl93E5uBmA-KsnfYy4PQ&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT-2bNT_-nmeTrXZ63RFsRZfLd1V5fMk3c1Eg&usqp=CAU"},
-{"result":   "https://i.ibb.co/CV2bDRK/FB-IMG-1616624680845.jpg"},
 {"result":  "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTpFPiCvBjKt-vwHXAXAXTXPs5OXS_htC1VHA&usqp=CAU"}
 ]
