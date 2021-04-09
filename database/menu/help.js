fa1a979a783c32101bdfcf0382d4d30d547e4135
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-04-09 17:22:58 -0500

    Add files via upload

---
+++ database/menu/help.js
@@ -0,0 +1,9 @@
+const help = (prefix, sender) => {
+        return `
+╔══✪〘 Iniciar uso del bot UwU 〙✪══
+║
+║───────⊹⊱✫⊰⊹───────
+║➩ 📌por fabor ingrese el comando *.ayuda* para ver los comandos disponibles 😇
+───────⊹⊱✫⊰⊹───────`
+}
+exports.help = help
