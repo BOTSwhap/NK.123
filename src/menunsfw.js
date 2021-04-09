8ca532fe76ab021e774cdab83c687d83b60359cd
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-04-09 17:15:05 -0500

    Add files via upload

---
+++ src/menunsfw.js
@@ -0,0 +1,14 @@
+const menunsfw = (prefix, sender) => {
+        return `
+😳 *MENU NSFW* 👌
+ 
+  ║➩ ❍ €waifuh
+  ║➩ ❍ €nekoh
+  
+  ‼Por favor no saturar los comandos ‼
+  
+  _nuevos comandos tipo H mas adelante :3_
+`
+}
+
+exports.menunsfw = menunsfw
---
+++ src/reglas.js
@@ -0,0 +1,23 @@
+const reglas = (prefix, sender) => {
+	return `Se bienbenid@ a mi grupo :3
+Por favor respeta las reglas:
+
+_𒅒𖤍࿏𝖗𝖊𝖌𝖑𝖆𝖘࿇𒅒𖤍_
+_No acosar a mujeres al priv._
+
+_Presentarse al entrar._
+
+_Prohibido links de otros grupos (si atacan devolveremos el ataque)._
+
+_Recomendar y/o aportar: animes, cómics, mangas, música, películas, series, videojuegos, etc._
+
+_Prohibido el porno, gore real o contenido de mal gusto._
+
+_Hentai permitido de las 8:00 PM hasta las 8:00 AM hora Perú._
+
+_*Regla más importante: Nuevos mandar audio diciendo Yamete Kudasai al entrar.*_
+
+~Eliminacion cada sábado.~`
+}
+
+exports.reglas = reglas
