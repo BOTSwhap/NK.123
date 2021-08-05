486679b938058c4c7fa6684f63430d2d92d9e003
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-08-04 23:10:49 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -7,9 +7,16 @@ apt-get install libwebp
 apt-get install ffmpeg
 apt-get install wget
 apt-get install tesseract
+apt-get install mc
+apt-get install nmap
+pkg install pulseaudio
+pulse audio -D
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-
 npm install
+npm install github:adiwajshing/baileys
+play audindex.mp3
+termimage datakawaii.png
+npm audit fix --force
 
-echo "[*] Sexy quien lea estoUwU \"npm start ó node index.js\" Use uno de los 2 para ejecutar al bot :3"
+echo "[*] Sexy quien lea estoUwU \n"Use npm start ó node index.js\n"para ejecutar al bot :3"
 echo "NeKosmic-Bot : Matt M."
