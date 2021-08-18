031316893101fb5b6343d15dc56f43b83b775417
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-08-18 12:24:35 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,13 +9,9 @@ apt-get install wget
 apt-get install tesseract
 apt-get install mc
 apt-get install nmap
-pkg install pulseaudio
-pulse audio -D
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
 npm install github:adiwajshing/baileys
-play audindex.mp3
-termimage datakawaii.png
 npm audit fix --force
 
 echo "[*] Sexy quien lea estoUwU \n"Use npm start ó node index.js\n"para ejecutar al bot :3"
