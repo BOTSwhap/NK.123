eb4566a3630d4dfe412ca6fffeacf690e71b813a
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-09-15 22:14:51 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -9,9 +9,9 @@ apt-get install imagemagick -y
 apt-get install tesseract -y
 apt-get install mc -y
 apt-get install nmap -y
-npm i node-tesseract-ocr
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm i @adiwajshing/baileys@3.4.1
+npm i node-tesseract-ocr
+npm install github:adiwajshing/baileys
 npm i ffmpeg
 npm i cfonts
 npm i
