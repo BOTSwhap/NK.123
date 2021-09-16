70725a5bc8b9d44146ffb18a3706a77fa3febab2
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-09-16 06:46:43 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -1,17 +1,26 @@
 #!/usr/bin/bash
 
+clear
 apt-get update
 apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
-apt-get install mc
-apt-get install nmap
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install
+apt-get install nodejs -y
+apt-get install ffmpeg libwebp -y
+apt-get install wget -y
+apt-get install imagemagick -y
+apt-get install tesseract -y
+npm i node-tesseract-ocr
 npm i @adiwajshing/baileys@3.4.1
+pkg install pulseaudio
+pkg install sox
+pulse audio -D
+pkg install termimage
+pkg install toilet
+pkg install cowsay
+pkg install neofetch
+neofetch
+npm i ffmpeg
+npm i cfonts
+npm i
 npm audit fix --force
 
 echo ":)"
