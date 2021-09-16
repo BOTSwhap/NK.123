4410f61098d7bba0d370af8c32ae50a2a84e2a56
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-09-15 22:27:13 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -11,8 +11,7 @@ apt-get install mc
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
-npm install github:adiwajshing/baileys
+npm i @adiwajshing/baileys@3.4.1
 npm audit fix --force
 
-echo "[*] Sexy quien lea estoUwU \n"Use npm start ó node index.js\n"para ejecutar al bot :3"
-echo "NeKosmic-Bot : Matt M."
+echo ":)"
