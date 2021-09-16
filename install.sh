7534ca8abb05cbff36411366e04582c81d0bc0fd
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-09-15 21:56:43 -0500

    Add files via upload

--- install.sh
+++ install.sh
@@ -2,17 +2,15 @@
 
 apt-get update
 apt-get upgrade
-apt-get install nodejs
-apt-get install libwebp
-apt-get install ffmpeg
-apt-get install wget
-apt-get install tesseract
-apt-get install mc
-apt-get install nmap
-wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
-npm install
-npm install github:adiwajshing/baileys
-npm audit fix --force
+apt-get install nodejs -y
+apt-get install ffmpeg libwebp -y
+apt-get install wget -y
+apt-get install imagemagick -y
+apt-get install tesseract -y
+npm i node-tesseract-ocr
+npm i @adiwajshing/baileys@3.4.1
+npm i ffmpeg
+npm i cfonts
+npm i
 
-echo "[*] Sexy quien lea estoUwU \n"Use npm start ó node index.js\n"para ejecutar al bot :3"
-echo "NeKosmic-Bot : Matt M."
+echo ":)"
