baeed98fe3ff4e8e75ef3bbdf88d66abc6738286
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-09-22 19:08:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,25 +3,15 @@
 clear
 apt-get update
 apt-get upgrade
-apt-get install nodejs -y
-apt-get install ffmpeg libwebp -y
-apt-get install wget -y
-apt-get install imagemagick -y
-apt-get install tesseract -y
-npm i node-tesseract-ocr
-npm i @adiwajshing/baileys@3.4.1
-pkg install pulseaudio
-pkg install sox
-pulse audio -D
-pkg install termimage
-pkg install toilet
-pkg install cowsay
-pkg install neofetch
-neofetch
-npm i ffmpeg
-npm i cfonts
-npm i
-npm audit fix --force
+apt-get install nodejs
+apt-get install libwebp
+apt-get install mc
+apt-get install ffmpeg
+apt-get install wget
+apt-get install tesseract
+apt-get install nmap
+wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
+npm install
 play audindex.mp3
 termimage datakawaii.png
 
