0699ad3cbd1fc43394bf790f08a29ab4ad756c7b
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-10-12 22:47:01 -0500

    Delete fakeapixd directory

--- fakeapixd/patasxd.js
+++
@@ -1,4 +0,0 @@
-[
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTZEY4qQmwAJhKVFlypt0nZNgzrkApOax6vew&usqp=CAU"},
-{"result": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfyqYAyrxqYswPhrZIfJz_wvS_56-zpPoWqg&usqp=CAU"}
-]
