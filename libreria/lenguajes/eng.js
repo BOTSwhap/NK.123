cb7203df9e612f33a7ba5f9fd75267fab9ef8821
NeKosmic <76775569+NeKosmic@users.noreply.github.com>
2021-10-12 22:52:25 -0500

    Add files via upload

---
+++ libreria/lenguajes/eng.js
@@ -0,0 +1,352 @@
+exports.panel = (prefix, time, fechanew) => {
+  return `*╔═「 _NK-BOT_ 」══*
+*╠「 Time-Zone 🌎 」*
+*║Peru          : ${time}*
+*║New-York : ${fechanew}*
+*╚═══════════*
+*╭┈┈┈┈┈┈┈┈┈┈┈*
+➱ *_COMMANDS_  ☷*
+*╰┈┈┈┈┈┈┈┈┈┈┈*
+*╔═══════════*
+*║ _CONVERTERS :_*
+*║╭—————————*
+*║├${prefix}sticker*
+*║├${prefix}voz*
+*║├${prefix}aimg*
+*║├${prefix}lector*
+*║├${prefix}ledstick*
+*║╰—————————*
+*╚═══════════*
+*╔═══════════*
+*║ _MULTIMEDIA :_*
+*║╭—————————*
+*║├${prefix}play*
+*║├${prefix}letra*
+*║├${prefix}random*
+*║├${prefix}playstore*
+*║├${prefix}imagen*
+*║├${prefix}pinterest*
+*║╰—————————*
+*╚═══════════*
+*╔═══════════*
+*║ _ADMINS CMDS :_*
+*║╭—————————*
+*║├${prefix}calumnia*
+*║├${prefix}invocar*
+*║├${prefix}invocar2*
+*║├${prefix}invocar3*
+*║├${prefix}invocar4*
+*║├${prefix}ascender*
+*║├${prefix}degradar*
+*║├${prefix}wlc*
+*║├${prefix}l*
+*║├${prefix}.*
+*║├${prefix}buscamsj*
+*║╰—————————*
+*╚═══════════*
+*╔═══════════*
+*║ _CREATOR CMDS :_*
+*║╭—————————*
+*║├${prefix}prefijoedit*
+*║├${prefix}ban* _~uso responsable~_
+*║├${prefix}bc*
+*║├${prefix}modo*
+*║├${prefix}banear*
+*║├${prefix}quitarbaneo*
+*║╰—————————*
+*╚═══════════*
+*╔═══════════*
+*║ _PASTIME :_*
+*║╭—————————*
+*║├${prefix}calumnia*
+*║├${prefix}simi*
+*║╰—————————*
+*╚═══════════*
+*╔═══════════*
+*║ _OTHER CMDS :_*
+*║╭—————————*
+*║├${prefix}bloqueados*
+*║├${prefix}baneados*
+*║├${prefix}admins*
+*║├${prefix}grupolink*
+*║├${prefix}estado*
+*║╰—————————*
+*╚═══════════*
+
+~Support me on my social networks~ ;3   ‏  ‌   ‏  ‌
+https://nekosmic.wordpress.com/
+`
+}
+exports.hora = () => {
+  return `Hours`
+}
+exports.minuto = () => {
+  return `Minutes`
+}
+exports.segundo = () => {
+  return `Seconds`
+}
+exports.nospam = () => {
+  return ` Please don't saturate the bot ;-;`
+}
+exports.nospam2 = (pushname) => {
+  return ` *[ ! ] My king ${pushname}*\n\_Please wait a few seconds before using another command_ ✓`
+}
+exports.opengp = () => {
+  return `🔓 *[ OPEN GROUP ]* 🔓`
+}
+exports.closegp = () => {
+  return `🔒 *[ CLOSED GROUP ]* 🔒`
+}
+exports.actualizacion1 = () => {
+  return `*[ THE DESCRIPTION OF THE GROUP WAS UPDATED ]*`
+}
+exports.actualizacion2 = () => {
+  return `*[ SETTINGS MADE BY THE GROUP ]*`
+}
+exports.actualizacion3 = () => {
+  return `*[ SETTINGS MADE BY THE GROUP ]*`
+}
+exports.apoyouwu = () => {
+  return `💻 *Social networks* 📲\n≫ Youtube Channel\n📌 https://youtube.com/channel/UC_Pp8pT9vbT15k5_-i6oseA ✔\n≫ Facebook page\n📌 https://fb.watch/6nGjVpIPsq/ ✔\n≫ Tiktok\n📌 https://vm.tiktok.com/ZMexrfdTY/ ✔ \n\nThank you very much for your support my king :3`
+}
+exports.apoyouwu2 = () => {
+  return `[ SUPPORT ✓]`
+}
+exports.bienbenida = () => {
+  return `Welcome`
+}
+exports.bienbenida2 = () => {
+  return `to this great group :`
+}
+exports.bienbenida3 = () => {
+  return `Please respect the group rules to avoid inconvenience :`
+}
+exports.admintrue = () => {
+  return `Now you are an admin congratulations my king `
+}
+exports.adminfalse = () => {
+  return ` You are no longer admin`
+}
+exports.despedida = () => {
+  return `Bye`
+}
+exports.nolink = () => {
+  return `*[ ! ]* A link was detected\nyou are an administrator, so I will not ban you from using links ;)`
+}
+exports.nolink2 = () => {
+  return `*Banished from the group, motive = Breaking a rule*\n*[ ! ]* _send a link_ *[ ! ]*`
+}
+exports.wait = () => {
+  return `_*Processing, please wait...*_ ⌛`
+}
+exports.waitt = () => {
+  return `_*Looking for result, wait...*_ 🔍`
+}
+exports.waittt = () => {
+  return `*_Wait toy processing your order..._*`
+}
+exports.success = () => {
+  return `*[✔]Completed*`
+}
+exports.stick = () => {
+  return `*[ ! ] _Sorry, could not be converted to sticker_*\n*[✓] _Try again_*`
+}
+exports.Iv = () => {
+  return `*[ X ] Invalid link [ X ]*`
+}
+exports.group = () => {
+  return `*[ ! ] _This command can only be used in groups!_*`
+}
+exports.benned = () => {
+  return `*[ ! ] _You are banned u.u_ [ ! ]*`
+}
+exports.ownerG = () => {
+  return `*[ ! ] _This command can only be used by the creator of the group!_*`
+}
+exports.ownerB = () => {
+  return `*[ ! ] _This command can only be used by the creator of the bot!_*`
+}
+exports.admin = () => {
+  return `*[ ! ] _This command can only be used by group administrators!_*`
+}
+exports.Badmin = () => {
+  return `*[ ! ] _The bot must be promoted to admin!_*`
+}
+exports.Bloqueads = () => {
+  return `*_List of contacts blocked by the bot [✓]_* `
+}
+exports.ocrc = () => {
+  return `*[ ! ] _Please use an image that contains text to be able to scan it :)_* `
+}
+exports.converror = () => {
+  return `*[ ! ] _Failed, at the time of conversion_* `
+}
+exports.converror2 = () => {
+  return `*[ ! ] _Failed, an error occurred, please try again in a few minutes_*`
+}
+exports.stickerror = (prefix) => {
+  return `*[ ! ] _Send/Reply an image using the text ${prefix}sticker_*`
+}
+exports.voztext = (prefix) => {
+  return `*[ ! ] _Prefix of the language in which you want to listen to the audio?_*\n\n_[✓] Use command ${prefix}idiomas to see the available languages_`
+}
+exports.voztext2 = () => {
+  return `*[ ! ] _Where is the text?_*`
+}
+exports.voztext3 = () => {
+  return `*[✓] _Accepted character limit: 600_*`
+}
+exports.voztext4 = () => {
+  return `*[ ! ] _Could not encode text_*`
+}
+exports.voztext5 = () => {
+  return `*After using the vos command, add a language type, here the available languages ✓* `
+}
+exports.predit = () => {
+  return `*[ ! ] _Please use only one symbol or letter_*`
+}
+exports.predit2 = (prefix) => {
+  return `*[✓] _The prefix was successfully changed_*\n_New prefix :_ *${prefix}*`
+}
+exports.fakemsj = (prefix) => {
+  return `how to use :\n${prefix}calumnia [@tag|mensage|answer]]\n\nExample : \n${prefix}calumnia @usuarioetiquetado|bendiceme|bendecido`
+}
+exports.alltag = () => {
+  return `*Summoner* `
+}
+exports.alltag2 = () => {
+  return ` *Invoking the members of the group* `
+}
+exports.alltag3 = (prefix) => {
+  return `Example of use : ${prefix}. sexy quien lo lea 7w7`
+}
+exports.alltag4 = (prefix) => {
+  return `How to use :\n${prefix}l video/imagen/audio...`
+}
+exports.Bcast = () => {
+  return `*[ ! ] _What is the message you want to convey?_*`
+}
+exports.Bcast2 = () => {
+  return `⚡ *Simultaneous message shared successfully :D*⚡`
+}
+exports.admin1 = () => {
+  return `*One participant was designated as administrator* \n`
+}
+exports.admin2 = () => {
+  return `*You were promoted to admin at* `
+}
+exports.admin3 = () => {
+  return ` *now has the power in the group! [✓]*`
+}
+exports.admin4 = () => {
+  return `*[ ! ] _Tag a participant who wants to remove their administration_* `
+}
+exports.admin5 = () => {
+  return `An administrator was discarded \n*F*\n`
+}
+exports.admin6 = () => {
+  return `*Degradation of power was executed by the administrator :* `
+}
+exports.admin7 = () => {
+  return `*Power was removed from* `
+}
+exports.elimin = () => {
+  return `*Tag a participant you want to remove* 🗡`
+}
+exports.admingod = () => {
+  return `*Here is the list of the great admins of the group* `
+}
+exports.gplinkk = () => {
+  return `*[HERE THE LINK OF THIS GREAT GROUP]* `
+}
+exports.saimg = () => {
+  return `*[ ! ] _Answer a sticker!_*`
+}
+exports.saimg2 = () => {
+  return `*[ ! ] _An error occurred, please try again_* ^_^`
+}
+exports.notext = () => {
+  return `where is the text?`
+}
+exports.wlcon = () => {
+  return `*Welcome activated in this group [✓]* `
+}
+exports.wlcoff = () => {
+  return `*Welcome disabled [ ! ]* `
+}
+exports.wlcon2 = () => {
+  return `_*[AUTOMATIC WELCOME]*_ `
+}
+exports.dlinkon = () => {
+  return `Anti-Link mode is activated in the group `
+}
+exports.dlinkoff = () => {
+  return `Anti-Link mode was disabled in the group `
+}
+exports.dlink = () => {
+  return `_*[ANTILINK MODE]*_ `
+}
+exports.mode = () => {
+  return `*[ MODES ] ✓*\n_PRIVATE / PUBLIC_ `
+}
+exports.mode2 = () => {
+  return `PRIVATE 🔒`
+}
+exports.mode3 = () => {
+  return `PUBLIC 🔓`
+}
+exports.mode4 = () => {
+  return `*[PUBLIC MODE ON]* 🔓\n_Now all users can use the bot ✓_ `
+}
+exports.mode5 = () => {
+  return `*[PRIVATE MODE ON]* 🔐\n_Now only the bot owner can use it ✓_`
+}
+exports.mode6 = () => {
+  return `*||| BOT STATUS |||* `
+}
+exports.banear = () => {
+  return `Successful ban to number : `
+}
+exports.banear2 = () => {
+  return ` Now you will no longer be able to use any commands`
+}
+exports.banear3 = () => {
+  return `》List of banned users《 : `
+}
+exports.banear4 = () => {
+  return `The ban was removed from the number `
+}
+exports.pint = () => {
+  return `*[ ! ] _What do you want to search on Pinterest?_*`
+}
+exports.gugul = () => {
+  return `*[ ! ] _What image do you want to find !?_*`
+}
+exports.storeapp = () => {
+  return `What application do you want to search in the Play Store?`
+}
+exports.next = () => {
+  return `< [ OTHER ] >`
+}
+exports.leds = (prefix) => {
+  return `*Example of use : ${prefix}ledst NeKosmic*`
+}
+exports.sherlok = () => {
+  return `*[ ! ] _What message do you want to search for within this group?_*`
+}
+exports.sherlok2 = () => {
+  return `*Found* `
+}
+exports.sherlok3 = () => {
+  return `*similar messages* \n\n_Collecting..._ `
+}
+exports.sherlok4 = () => {
+  return `*[ ! ] _Sorry I found no similarities for this message_*`
+}
+exports.letter = () => {
+  return `*[ ! ] _What do you want to look for?_*`
+}
+exports.playaudi = (prefix) => {
+  return `Example: ${prefix}play mtc s3rl`
+}
---
+++ libreria/lenguajes/nexo.js
@@ -0,0 +1,4 @@
+exports.eng = require('./eng.js') //English language
+exports.ind = require('./ind.js') //bahasa Indonesia
+exports.ptg = require('./ptg.js') //idioma portugues 
+exports.esp = require('./esp.js') //Lenguaje español
