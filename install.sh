c24db943b2bf1310e680c3911e156538a8ba89fa
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-01-10 09:20:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,6 +12,7 @@ apt-get install tesseract
 apt-get install nmap
 wget -O ~/../usr/share/tessdata/ind.traineddata "https://github.com/tesseract-ocr/tessdata/blob/master/ind.traineddata?raw=true"
 npm install
+npm i colors@1.4.0
 play audindex.mp3
 termimage datakawaii.png
 
