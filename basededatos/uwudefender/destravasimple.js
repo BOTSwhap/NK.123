1b23c14a493932512f80c0ae4367942fe45e71aa
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-03-30 21:52:58 -0500

    Delete destravasimple.js

--- basededatos/uwudefender/destravasimple.js
+++
@@ -1,6 +0,0 @@
-const kuakerzzz = () => {
-        return `Marquen como leido el chat \n\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n⚡ *UwU* ⚡\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》\n《\n 》
-`
-}
-
-exports.kuakerzzz = kuakerzzz
