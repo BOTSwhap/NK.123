a70af26b2fa21682a2c8c6695053bbaad7f7742b
Matt_Melendez <76775569+NeKosmic@users.noreply.github.com>
2022-03-30 21:54:06 -0500

    Delete libreria/lenguajes directory

--- libreria/lenguajes/nexo.js
+++
@@ -1,4 +0,0 @@
-exports.eng = require('./eng.js') //English language
-exports.ind = require('./ind.js') //bahasa Indonesia
-exports.ptg = require('./ptg.js') //idioma portugues 
-exports.esp = require('./esp.js') //Lenguaje español
